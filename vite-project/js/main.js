92408e8eeea5170de98ea56cf164b1d55003e6f0
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2022-12-02 10:16:41 -0500

    .

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,11 +1,10 @@
 import "../styles/style.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
-AOS.init();
-import { menu } from "./menu";
-document.querySelector("#app").innerHTML = `
-<h1>Hello Vite!!!</h1>
-<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
-
-console.log(menu);
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
