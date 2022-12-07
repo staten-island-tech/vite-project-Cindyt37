9244a5ec6c7904dd8fb67594912fd5c2e5cb6353
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2022-12-07 10:16:32 -0500

    .

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,4 +1,5 @@
 import "../styles/style.css";
+
 document.querySelector(".btn").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
     document.body.classList.add("dark");
